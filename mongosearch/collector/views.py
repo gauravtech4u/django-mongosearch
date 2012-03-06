e90e0519cf684a669f1689d4b7b87a18ced77667
Rahul Agarwal <agarwalrahul.1986@gmail.com>
2012-03-06 10:33:59 +0530

    MongoDB creation from JSON Dump

--- mongosearch/collector/views.py
+++ mongosearch/collector/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from mongosearch.collector.mongodb import CreateMongoCollection
+
+def  upload_form( request ):
+    obj = CreateMongoCollection( request ).upload_form()
+    return obj
+
+def  create_db( request ):
+    obj = CreateMongoCollection( request ).create_db()
+    return obj
