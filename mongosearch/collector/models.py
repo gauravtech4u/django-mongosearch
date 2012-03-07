867c7d50be9f98f5b2de950ae5063d1fe16c3cf3
Gaurav Kapoor <mail.gauravkapoor@gmail.com>
2012-03-07 16:56:17 +0530

    wrapper collection queries added

--- mongosearch/collector/models.py
+++ mongosearch/collector/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from mongosearch.core.connection import db
 
-# Create your models here.
+
+class CollectionWrapper(object):
+ 
+    def load_json(self,kwargs):
+        self.db_col.insert(kwargs)
+    
+    def find_one(self,kwargs):
+        return self.db_col.find_one(kwargs)
+        
+    def find(self,kwargs):
+        return self.db_col.find(kwargs)
+        
+    def collection_names(self,kwargs):
+        return self.db_col.collection_names(kwargs)
+    
+    def save(self,kwargs):
+        self.db_col.find(kwargs)
+    
+class CollectionMapping(CollectionWrapper):
+
+    def __init__(self,model):
+        self.db_col = db[model]
+    
